55553910ac6fdb5d843faef80bfa5465cd735ffc
Baurain Olivier <baurain.o@gmail.com>
2016-11-11 19:14:40 +0100

    Reading the server JSON

---
+++ js/threeCanvas.js
@@ -0,0 +1,40 @@
+var stats = new Stats();
+var scene = new THREE.Scene();
+var camera = new THREE.PerspectiveCamera( 75, window.innerWidth/window.innerHeight, 0.1, 1000 );
+
+var renderer = new THREE.WebGLRenderer();
+renderer.setSize( window.innerWidth/2, window.innerHeight/2 );
+document.getElementById('gameCanvas').appendChild( renderer.domElement );
+document.getElementById('gameCanvas').appendChild( stats.dom );
+
+
+
+camera.position.z = 5;
+
+var render = function () {
+  requestAnimationFrame( render );
+
+  update()
+  stats.update();
+  renderer.render(scene, camera);
+};
+
+render();
+
+
+function update() {
+  for (var i = 0; i < engineObjects.length; i++) {
+    var tmpObject = scene.getObjectByName(engineObjects[i].name);
+    if (tmpObject) {
+      tmpObject.position.fromArray(engineObjects[i].position);
+      console.log("ICI");
+    }
+    else {
+      var geometry = new THREE.SphereGeometry( 1, 15, 15 );
+      var material = new THREE.MeshBasicMaterial( { color: 0x00ff00 } );
+      var sphere = new THREE.Mesh( geometry, material );
+      sphere.name = engineObjects[i].name;
+      scene.add( sphere );
+    }
+  }
+}
---
+++ js/websocket.js
@@ -0,0 +1,81 @@
+    var engineObjects = []
+    var debugTextArea = document.getElementById("debugTextArea");
+    function debug(message) {
+        debugTextArea.value += message + "\n";
+        debugTextArea.scrollTop = debugTextArea.scrollHeight;
+    }
+
+    function sendMessage() {
+        var msg = document.getElementById("inputText").value;
+        if ( websocket != null )
+        {
+            document.getElementById("inputText").value = "";
+            websocket.send( msg );
+            console.log( "string sent :", '"'+msg+'"' );
+        }
+    }
+
+    var wsUri = "ws://localhost:9002";
+    var websocket = null;
+    initWebSocket();
+    function initWebSocket() {
+        try {
+            if (typeof MozWebSocket == 'function')
+                WebSocket = MozWebSocket;
+            if ( websocket && websocket.readyState == 1 )
+                websocket.close();
+            websocket = new WebSocket( wsUri );
+            websocket.onopen = function (evt) {
+                debug("CONNECTED");
+            };
+            websocket.onclose = function (evt) {
+                debug("DISCONNECTED");
+            };
+            websocket.onmessage = function (evt) {
+                engineObjects = JSON.parse(evt.data);
+                console.log(engineObjects);
+                debug( evt.data );
+            };
+            websocket.onerror = function (evt) {
+                debug('ERROR: ' + evt.data);
+            };
+        } catch (exception) {
+            debug('ERROR: ' + exception);
+        }
+    }
+
+    function stopWebSocket() {
+        if (websocket)
+            websocket.close();
+    }
+
+    function checkSocket() {
+        if (websocket != null) {
+            var stateStr;
+            switch (websocket.readyState) {
+                case 0: {
+                    stateStr = "CONNECTING";
+                    break;
+                }
+                case 1: {
+                    stateStr = "OPEN";
+                    break;
+                }
+                case 2: {
+                    stateStr = "CLOSING";
+                    break;
+                }
+                case 3: {
+                    stateStr = "CLOSED";
+                    break;
+                }
+                default: {
+                    stateStr = "UNKNOW";
+                    break;
+                }
+            }
+            debug("WebSocket state = " + websocket.readyState + " ( " + stateStr + " )");
+        } else {
+            debug("WebSocket is null");
+        }
+    }
